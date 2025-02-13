b82b787810e5b47bf6a827fb9b04e14046eeb251
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-02-13 16:14:54 +0700

    [FIX] logs

--- config/database.go
+++ config/database.go
@@ -15,12 +15,14 @@ var DB *pgxpool.Pool
 func ConnectDB() {
 	err := godotenv.Load()
 	if err != nil {
+		log.Println(err)
 		log.Fatal("Error loading .env file")
 	}
 
 	databaseUrl := os.Getenv("DATABASE_URL")
 	dbpool, err := pgxpool.New(context.Background(), databaseUrl)
 	if err != nil {
+		log.Println(err)
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 
